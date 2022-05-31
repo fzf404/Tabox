60f406faae28783299057f16ea287170cf2654b5
你们的小f <nmdfzf404@163.com>
2022-05-31 22:49:45 +0800

    :recycle: 重构 Hooks

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,7 @@
 /*
  * @Author: fzf404
  * @Date: 2022-04-23 19:52:16
- * @LastEditTime: 2022-05-11 19:10:44
+ * @LastEditTime: 2022-05-31 22:46:30
  * @Description: 主页
  */
 import { useState, useEffect } from 'react'
@@ -19,6 +19,7 @@ import {
   Input,
   Checkbox,
   Drawer,
+  Switch,
   Row,
   Col,
   PageHeader,
@@ -40,43 +41,128 @@ const { Search } = Input
 const { Meta } = Card
 
 export default function App() {
-  // config 加载中
-  const [loaded, setLoaded] = useState(false)
-  // github 加载中
-  const [githubLoaded, setGithubLoaded] = useState(false)
-
-  // 设置菜单
-  const [setting, setSetting] = useState(false)
-  // 设置错误提醒
-  const [settingError, setSettingError] = useState(false)
-
-  const onSettingClose = () => {
-    setSetting(false)
-  }
-  // 配置数据
-  const [config, setConfig] = useState({})
-  // 代码原始数据
-  const [configRaw, setConfigRaw] = useState('')
-  // 配置地址
-  // const [configURL, setConfigURL] = useState('')
+  // 配置信息
+  const [config, setConfig] = useState({
+    loading: true,
+    url: 'config.yaml',
+    yaml: '',
+    json: {},
+  })
 
   // 获取配置数据
   useEffect(() => {
-    // 读取配置文件
+    // 请求配置文件
     const fetchConfig = async () => {
-      const res = await fetch('config.yaml')
-      const raw = await res.text()
-      setConfigRaw(raw) // 写入原始数据
+      const res = await fetch(config.url)
+      const text = await res.text()
+      setConfig(...config, { yaml: text, json: YAML.parse(text) })
     }
-    // 读取本地配置
-    const localConfig = localStorage.getItem('config')
-    if (localConfig) {
-      const JSONConfig = JSON.parse(localConfig)
-      setConfigRaw(YAML.stringify(JSONConfig))
-    } else {
-      fetchConfig() // 发起请求
+
+    // 加载配置文件
+    const loadConfig = () => {
+      const configRaw = localStorage.getItem('tabox-config')
+      const configJSON = JSON.parse(configRaw)
+      setConfig(...config, { yaml: YAML.stringify(configJSON), json: configJSON })
     }
+
+    // 是否为编辑模式
+    localStorage.getItem('tabox-edit-mode') ? loadConfig() : fetchConfig()
   }, [])
+
+  // 侧栏展示状态
+  const [showNav, setShowNav] = useState(false)
+
+  // 搜索
+  const [search, setSearch] = useState({
+    checkedMenu: [],
+    checkedKey: [],
+  })
+
+  // Github 信息
+  const [github, setGithub] = useState({
+    loading: true,
+    data: [],
+  })
+
+  // config 更改后更新配置
+  useEffect(() => {
+    if (Object.keys(config).length) {
+      // 写入默认折叠状态
+      setShowNav(config.json.Config.hide)
+      // 处理侧边栏菜单
+      // setMenuItems(config.json.Tabox)
+      // 配置默认搜索菜单
+      // setSearchMenu(Object.keys(config.json.Search)[0])
+      // 配置默认选中搜索菜单
+      setSearchKeys(Object.keys(config.json.Search[Object.keys(config.json.Search)[0]]))
+      // 配置默认选中搜索范围
+      setSearchChecked([Object.keys(config.json.Search[Object.keys(config.json.Search)[0]])[0]])
+      // config 加载成功
+      setConfig(...config, { loading: false })
+      // 配置 github
+      if (config.json.Tabox.Github !== undefined) {
+        fetch(`https://api.github.com/users/${config.json.Tabox.Github.name}/repos?per_page=100`)
+          .then((res) => res.json())
+          .then((data) => {
+            let repoInfo = data
+            repoInfo.sort((a, b) => {
+              return b.stargazers_count - a.stargazers_count
+            })
+            // 设置 Github 信息
+            setGithub(...github, { data: repoInfo, loading: false })
+            // setGithubItems(repoInfo)
+            // Github 加载成功
+            // setGithubLoaded(true)
+          })
+      }
+    }
+  }, [config])
+
+  // 设置菜单
+  const [setting, setSetting] = useState({
+    show: false,
+    error: false,
+    edit: false, // 编辑模式
+    raw: '',
+  })
+
+  // 关闭设置
+  const onSettingClose = () => {
+    setSetting(...setting, { open: false })
+  }
+
+  // config 加载中
+  // const [loaded, setLoaded] = useState(false)
+  // github 加载中
+  // const [githubLoaded, setGithubLoaded] = useState(false)
+
+  // 设置菜单
+  // const [setting, setSetting] = useState(false)
+  // 设置错误提醒
+  // const [settingError, setSettingError] = useState(false)
+
+  // github 信息
+  // const [githubItems, setGithubItems] = useState([])
+
+  // 侧边栏折叠
+  // const [collapsed, setCollapsed] = useState(false)
+
+  // // 标签组内容
+  // const [menuItems, setMenuItems] = useState([])
+
+  // 选中的搜索菜单
+  // const [searchMenu, setSearchMenu] = useState([])
+  // 搜索菜单内容
+  // const [searchKey, setSearchKeys] = useState([])
+  // 选中的搜索内容
+  // const [searchChecked, setSearchChecked] = useState([])
+
+  // 配置数据
+  // const [config, setConfig] = useState({})
+  // 代码原始数据
+  // const [configRaw, setConfigRaw] = useState('')
+  // 配置地址
+  // const [configURL, setConfigURL] = useState('')
 
   // 配置文件更改
   const onConfigChange = (value) => {
@@ -94,74 +180,17 @@ export default function App() {
     localStorage.setItem('config', JSON.stringify(parse))
   }
 
-  // 解析配置数据
-  useEffect(() => {
-    const parse = YAML.parse(configRaw)
-    if (parse) {
-      // 写入配置信息
-      setConfig(parse)
-    }
-  }, [configRaw])
-
-  // github 信息
-  const [githubItems, setGithubItems] = useState([])
-
-  // config 更改后更新配置
-  useEffect(() => {
-    if (Object.keys(config).length) {
-      // 写入默认折叠状态
-      setCollapsed(config.Config.hide)
-      // 处理侧边栏菜单
-      setMenuItems(config.Tabox)
-      // 配置默认搜索菜单
-      setSearchMenu(Object.keys(config.Search)[0])
-      // 配置默认选中搜索菜单
-      setSearchKeys(Object.keys(config.Search[Object.keys(config.Search)[0]]))
-      // 配置默认选中搜索范围
-      setSearchChecked([Object.keys(config.Search[Object.keys(config.Search)[0]])[0]])
-      // config 加载成功
-      setLoaded(true)
-      // 配置 github
-      if (config.Tabox.Github !== undefined) {
-        fetch(`https://api.github.com/users/${config.Tabox.Github.name}/repos?per_page=100`)
-          .then((res) => res.json())
-          .then((data) => {
-            let repoInfo = data
-            repoInfo.sort((a, b) => {
-              return b.stargazers_count - a.stargazers_count
-            })
-            setGithubItems(repoInfo)
-            // Github 加载成功
-            setGithubLoaded(true)
-          })
-      }
-    }
-  }, [config])
-
-  // 侧边栏折叠
-  const [collapsed, setCollapsed] = useState(false)
-
-  // 标签组内容
-  const [menuItems, setMenuItems] = useState([])
-
-  // 选中的搜索菜单
-  const [searchMenu, setSearchMenu] = useState([])
-  // 搜索菜单内容
-  const [searchKey, setSearchKeys] = useState([])
-  // 选中的搜索内容
-  const [searchChecked, setSearchChecked] = useState([])
-
   // 切换搜索菜单
   const onSearchChange = (e) => {
     setSearchMenu(e.key)
-    setSearchKeys(Object.keys(config.Search[e.key]))
+    setSearchKeys(Object.keys(config.json.Search[e.key]))
     setSearchChecked([])
   }
 
   // 搜索
   const onSearch = (e) => {
     searchChecked.forEach((key) => {
-      window.open(config.Search[searchMenu][key] + e)
+      window.open(config.json.Search[searchMenu][key] + e)
     })
   }
 
@@ -181,7 +210,9 @@ export default function App() {
     document.getElementById(e.key).scrollIntoView({ block: 'center', behavior: 'smooth' })
   }
 
-  return loaded ? (
+  return loading.config ? (
+    <Spin tip="Loading" size="large" style={{ width: '100vw', height: '100vh', marginTop: '16rem' }} />
+  ) : (
     <Layout
       style={{
         minHeight: '100vh',
@@ -195,11 +226,11 @@ export default function App() {
         style={{ position: 'fixed', height: '100vh' }}>
         {/* 网站标题 */}
         <Space direction="vertical" size="middle" style={{ margin: '1.3rem 1.3rem 0' }}>
-          <a target="_blank" href={config.Config.link} rel="noreferrer">
-            <Avatar shape="square" size="large" src={config.Config.logo} />
+          <a target="_blank" href={config.json.Config.link} rel="noreferrer">
+            <Avatar shape="square" size="large" src={config.json.Config.logo} />
           </a>
           <Title level={2} style={{ color: '#eee', display: collapsed ? 'none' : '' }}>
-            {config.Config.title}
+            {config.json.Config.title}
           </Title>
         </Space>
         {/* 侧边栏菜单 */}
@@ -218,7 +249,11 @@ export default function App() {
         </Menu>
       </Sider>
       {/* 内容区 */}
-      <Layout style={{ marginLeft: collapsed ? '80px' : '220px', transition: collapsed ? 'margin-left 200ms': 'margin-left 400ms'}}>
+      <Layout
+        style={{
+          marginLeft: collapsed ? '80px' : '220px',
+          transition: collapsed ? 'margin-left 200ms' : 'margin-left 400ms',
+        }}>
         {/* 顶部导航 */}
         <Header style={{ backgroundColor: '#abc' }}>
           {/* 时间 */}
@@ -303,7 +338,7 @@ export default function App() {
               mode="horizontal"
               selectedKeys={searchMenu}
               onClick={onSearchChange}>
-              {Object.keys(config.Search).map((searchMenuKey) => {
+              {Object.keys(config.json.Search).map((searchMenuKey) => {
                 return <Menu.Item key={searchMenuKey}>{searchMenuKey}</Menu.Item>
               })}
             </Menu>
@@ -336,8 +371,8 @@ export default function App() {
           {/* 标签页内容 */}
           <div>
             {/* 遍历标签组 */}
-            {Object.keys(config.Tabox).map((tabKey) => {
-              const menuBox = config.Tabox[tabKey]
+            {Object.keys(config.json.Tabox).map((tabKey) => {
+              const menuBox = config.json.Tabox[tabKey]
               return (
                 <div
                   id={tabKey}
@@ -376,11 +411,11 @@ export default function App() {
                           ) {
                             return ''
                           }
-                          const tabItem = config.Tabox[tabKey][boxKey]
+                          const tabItem = config.json.Tabox[tabKey][boxKey]
                           // github 渲染
                           if (tabKey === 'Github' && boxKey === 'name') {
                             // 获取忽略的仓库
-                            const ignoreItems = config.Tabox.Github.Ignore
+                            const ignoreItems = config.json.Tabox.Github.Ignore
                             // 判断 GIthub 仓库是否加载成功
                             return githubLoaded ? (
                               githubItems.map((githubItem) => {
@@ -477,7 +512,5 @@ export default function App() {
         </Footer>
       </Layout>
     </Layout>
-  ) : (
-    <Spin tip="Loading" size="large" style={{ width: '100vw', height: '100vh', marginTop: '16rem' }} />
   )
 }
